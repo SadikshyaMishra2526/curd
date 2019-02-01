38021526919b60458459aeb0f4c4400a697be75a
SadikshyaMishra2526 <sadikshyamishra25@gmail.com>
2019-02-01 16:12:38 +0545

    'adding'

---
+++ resources/views/Front/layouts/master.blade.php
@@ -0,0 +1,39 @@
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title')</title>
+
+    <link href='https://fonts.googleapis.com/css?family=Raleway:400,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/custom.css">
+    @yield('style');
+</head>
+<body>
+  @include('Front.layouts.header')
+
+  @yield('posts')
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <p class="copyright">&copy; 2016 Edo Masaru</p>
+                </div>
+                <div class="col-md-4">
+                    <nav>
+                        <ul class="social-icons">
+                            <li><a href="#" class="i fa fa-facebook"></a></li>
+                            <li><a href="#" class="i fa fa-twitter"></a></li>
+                            <li><a href="#" class="i fa fa-google-plus"></a></li>
+                            <li><a href="#" class="i fa fa-github"></a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script src="js/bootstrap.min.js"></script>
+    @yield('script')
+</body>
+</html>
